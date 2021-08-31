7cd99256ea0ad482764023ef0399dc5788d91ae3
yedong <yedong@huya.com>
2021-08-31 21:33:28 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import * as React from "react";
+import { TEXT } from "@/utils";
 export default function App(): JSX.Element {
-  return <div className="App">hello world</div>;
+  React.useEffect(() => {
+    document.title = import.meta.env.VITE_TITLE;
+  }, []);
+  return (
+    <>
+      <div className="App">hello world~~~~~{TEXT}</div>
+      <div>{TEXT?.a?.b?.c}</div>
+      <div>{["DEMO", "TEXT"].includes(TEXT) ? "includes" : "none"}</div>
+      <div>
+        {["DEMO", "TEXT"].find((key) => key === TEXT)
+          ? "find includes"
+          : "none"}
+      </div>
+    </>
+  );
 }
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,30 @@
-import { UserConfigFn } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import legacy from '@vitejs/plugin-legacy';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import reactJsx from 'vite-react-jsx';
-import windicss from 'vite-plugin-windicss';
-import { visualizer } from 'rollup-plugin-visualizer';
-import mkcert from 'vite-plugin-mkcert';
-
+import { UserConfigFn } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import legacy from "@vitejs/plugin-legacy";
+import tsconfigPaths from "vite-tsconfig-paths";
+import reactJsx from "vite-react-jsx";
+import { visualizer } from "rollup-plugin-visualizer";
 const defineConfig: UserConfigFn = ({ command, mode }) => {
   const config = {
-    server: {
-      https: true,
-    },
     plugins: [
-      windicss(),
       reactRefresh(),
       tsconfigPaths(),
-      legacy(),
-      reactJsx(),
-      mkcert({
-        source: 'coding',
+      legacy({
+        targets: ["ie >= 11"],
+        additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
       }),
+      reactJsx(),
     ],
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ['react'],
-            "react-dom": ["react-dom"]
-          },
-        },
-      },
-    },
+    // rollupOptions: {
+    //   input: {
+    //     main: resolve(__dirname, "index.html"),
+    //   },
+    // },
   };
-  if (mode === 'analyze') {
+  if (mode === "analyze") {
     config.plugins.push(
       visualizer({
-        filename: './node_modules/.cache/visualizer/stats.html',
+        filename: "./node_modules/.cache/visualizer/stats.html",
         open: true,
         gzipSize: true,
         brotliSize: true,
